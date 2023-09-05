48aae77c5ade6f52e21498b42e57d3d7ba984666
Ronojith Saha Roy Dipta <48882037+docRoy-Dipta@users.noreply.github.com>
2023-09-06 03:45:50 +0600

    Update urls.py

--- eLMS/urls.py
+++ eLMS/urls.py
@@ -37,3 +37,5 @@ urlpatterns = [
     path('', include('quiz.urls')),
     path('froala_editor/', include('froala_editor.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
